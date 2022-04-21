ba57e3f6f7cdb46ad6beeb1f2f27a34f4d48e7ef
Papuna Gagnidze <pgagnidze@pm.me>
2022-04-21 17:34:17 +0400

    Added a16z portfolio

--- src/data/ventures.ts
+++ src/data/ventures.ts
@@ -2,6 +2,7 @@ const ventures = [
   { name: 'Coinbase Ventures Portfolio', slug: 'coinbase-ventures-portfolio' },
   { name: 'Huobi Capital Portfolio', slug: 'huobi-capital-portfolio' },
   { name: 'Binance Labs Portfolio', slug: 'binance-labs-portfolio' },
+  { name: 'a16z Portfolio', slug: 'a16z-portfolio' },
 ]
 
 export default ventures
